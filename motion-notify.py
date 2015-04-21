3e9461b488643393f82941be168e9c77893a4490
Charles Butler <chuck@dasroot.net>
2015-04-21 01:18:15 -0400

    Strip the Google dependency from motion.py
    Insert Dropbox and Pushbullet dependencies.
    
    Refactor out the MAC address detection, it was discouraged so just gut
    it.
    
    Changed the logic for determining if within a 'guard' window - prior set
    to SystemON/Off settings. I feel the 'Guard' notation makes it obvious
    that we are guarding ourselves during certain hours of the day - send me
    notices while i'm sleeping for example.
    
    Break out the sys.argv[] blocks for argparse, i prefer flags to
    positional arguments myself. Give them sane defaults and let the app try
    to do its best.
    
    Dropbox upload / pushbullet notify methods created.

--- motion-notify.py
+++ motion-notify.py
@@ -35,267 +35,196 @@ pypymotion (https://github.com/7AC/pypymotion) by Wayne Dyck
 # You should have received a copy of the GNU General Public License
 # along with Motion Notify.  If not, see <http://www.gnu.org/licenses/>.
 
-import smtplib
 from datetime import datetime
 
 import os.path
 import sys
 import subprocess
-# import time
+import argparse
 
-import gdata.data
-import gdata.docs.data
-import gdata.docs.client
 import ConfigParser
-import atom.data
 
 import logging.handlers
 import traceback
 
-logger = logging.getLogger('MotionNotify')
+# Service libs
+import dropbox
+from pushbullet import Pushbullet
+
+log = logging.getLogger('MotionNotify')
 hdlr = logging.handlers.RotatingFileHandler('/var/tmp/motion-notify.log',
                                             maxBytes=1048576,
                                             backupCount=3)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
-logger.setLevel(logging.INFO)
+log.addHandler(hdlr)
+log.setLevel(logging.INFO)
 
 
-def loggerExceptHook(t, v, tb):
-    logger.error(traceback.format_exception(t, v, tb))
+def logExceptHook(t, v, tb):
+    log.error(traceback.format_exception(t, v, tb))
 
-sys.excepthook = loggerExceptHook
+sys.excepthook = logExceptHook
 
 
 class MotionNotify:
 
-    def __init__(self, config_file_path):
-        logger.info("Loading config")
+    def __init__(self, config_file_path, notify):
+        log.info("Loading config")
         # Load config
         config = ConfigParser.ConfigParser()
         config.read(config_file_path)
-        logger.info("Config file read")
-        # GMail account credentials
-        self.username = config.get('gmail', 'user')
-        self.password = config.get('gmail', 'password')
-        self.from_name = config.get('gmail', 'name')
-        self.sender = config.get('gmail', 'sender')
 
-        # Recipient email address (could be same as from_addr)
-        self.recipient = config.get('gmail', 'recipient')
+        # [zone]
+        self.region = config.get('zone', 'region')
+        # Delete the local video file after the upload
+        self.cleanup = config.get('zone', 'cleanup')
 
-        # Subject line for email
-        self.subject = config.get('gmail', 'subject')
+        # set sleeping hours
+        self.zone_guard_start = 1
+        self.zone_guard_end = 7
 
-        # First line of email message
-        self.message = config.get('gmail', 'message')
+        # [pushbullet]
+        # disable pushbullet notifications if false
+        self.pushbullet_enabled = config.get('pushbullet', 'enabled')
+        # pushbullet API Key - obtain from: https://www.pushbullet.com/account
+        self.apikey = config.get('pushbullet', 'apikey')
+        # Subject line for notification message
+        self.subject = config.get('pushbullet', 'subject')
+        # First line of notification message
+        self.message = config.get('pushbullet', 'message')
+        self.event_message = config.get('pushbullet', 'event_message')
+        if self.pushbullet_enabled and notify:
+           self._create_pushbullet_client()
 
-        # Folder (or collection) in Docs where you want the media to go
-        self.folder = config.get('docs', 'folder')
+        # [dropbox]
+        # Folder in Dropbox where you want the videos to go
+        self.folder = config.get('dropbox', 'folder')
+        self.folder_link = config.get('dropbox', 'folder_link')
+        self.access_token = config.get('dropbox', 'access_token')
+        self._create_dropbox_client()
 
-        # Options
-        self.delete_files = config.getboolean('options', 'delete-files')
-        self.send_email = config.getboolean('options', 'send-email')
-
-        self._create_gdata_client()
-
-        self.google_drive_folder_link = config.get('gmail', 'google_drive_folder_link')
-        self.event_started_message = config.get('gmail', 'event_started_message')
-
+        # set defaults for LAN config - this is shakey at best
         self.presenceMacs = []
         self.network = None
-
         self.ip_addresses = None
 
+        #[LAN]
         try:
+            # MAC addresses (comma separated) - override with ip_addresses
             self.presenceMacs = config.get('LAN', 'presence_macs').split(',')
+            # Network to monitor (used by MAC address detection)
             self.network = config.get('LAN', 'network')
         except ConfigParser.NoSectionError, ConfigParser.NoOptionError:
             pass
 
         try:
-            self.ip_addresses = config.get('LAN', 'ip_addresses')
+            #Space separated list of IP addresses
+            self.ip_addresses = config.get('LAN', 'ip_addresses').split(',')
         except ConfigParser.NoSectionError, ConfigParser.NoOptionError:
             pass
 
-        try:
-            self.forceOnStart = config.getint('activate-system', 'force_on_start')
-            self.forceOnEnd = config.getint('activate-system', 'force_on_end')
-        except ConfigParser.NoSectionError, ConfigParser.NoOptionError:
-            pass
 
-        logger.info("All config options set")
+    def _create_dropbox_client(self):
+        ''' Allocate a DropBox client for re-use '''
+        self.dropbox = dropbox.client.DropboxClient(self.access_token)
+        log.info("linked account: {}".format(self.dropbox.account_info()))
 
-    def _create_gdata_client(self):
-        """Create a Documents List Client."""
-        self.client = gdata.docs.client.DocsClient(source='motion_uploader')
-        self.client.http_client.debug = False
-        self.client.client_login(self.username, self.password, service=self.client.auth_service, source=self.client.source)
+    def _upload_dropbox_file(self, filepath):
+        ''' Upload any file passed to this method '''
+        filename = filepath.split('/')[-1]
+        dropbox_file = "/{}/{}".format(self.folder,filename)
+        with open(filepath, 'rb') as f:
+           response = self.dropbox.put_file(dropbox_file, f)
+        return response
 
-    def _get_folder_resource(self):
-        try:
-            resources = self.client.GetAllResources(uri='https://docs.google.com/feeds/default/private/full/-/folder?title=' + self.folder + '&title-exact=true')
-            return resources[0]
-        except:
-            sys.exit('ERROR: Unable to retrieve resources')
+    def _create_pushbullet_client(self):
+        self.pushbullet = Pushbullet(self.apikey)
+        log.info("initialized pushbullet client")
 
-    def _send_email(self, msg):
-        '''Send an email using the GMail account.'''
-        senddate = datetime.strftime(datetime.now(), '%Y-%m-%d')
-        m = "Date: %s\r\nFrom: %s <%s>\r\nTo: %s\r\nSubject: %s\r\nX-Mailer: My-Mail\r\n\r\n" % (senddate, self.from_name, self.sender, self.recipient, self.subject)
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.starttls()
-        server.login(self.username, self.password)
-        server.sendmail(self.sender, self.recipient, m+msg)
-        server.quit()
-
-    def _upload(self, media_file_path, folder_resource):
-        '''Upload the media and return the doc'''
-        if media_file_path.endswith('jpg'):
-            logger.info('Uploading image %s ' % media_file_path)
-            try:
-                image_file = open(media_file_path)
-            except IOError, e:
-                sys.exit('ERROR: Unable to open ' + media_file_path + ': ' + e[1])
-
-            file_size = os.path.getsize(image_file.name)
-            file_type = "image/jpeg"
-            uri = folder_resource.get_resumable_create_media_link().href
-            uri += '?convert=false'
-            uploader = gdata.client.ResumableUploader(self.client, image_file, file_type, file_size, chunk_size=1048576, desired_class=gdata.data.GDEntry)
-            doc = uploader.UploadFile(uri, entry=gdata.data.GDEntry(title=atom.data.Title(text=os.path.basename(image_file.name))))
-
-        else:
-            if media_file_path.endswith(('avi', 'flv', 'mov', 'mpg', 'swf')):
-                logger.info('Uploading video %s ' % media_file_path)
-                doc = gdata.docs.data.Resource(type='video', title=os.path.basename(media_file_path))
-                media = gdata.data.MediaSource()
-                media.SetFileHandle(media_file_path, 'video/avi')
-                doc = self.client.CreateResource(doc, media=media, collection=folder_resource)
-
-        return doc
+    def _push_notice(self, msg):
+        ''' Pushes a notice via pushbullet '''
+        if not self.pushbullet:
+            return
+        import ipdb; ipdb.set_trace()
+        self.pushbullet.push_note(self.subject.format(self.region),
+                                  msg.format(self.region))
 
     def _system_active(self):
-        now = datetime.now()
-        system_active = True
-        # Ignore presence if force_on specified
-        if self.forceOnStart and self.forceOnEnd and \
-            now.hour >= self.forceOnStart and now.hour < self.forceOnEnd:
-            logger.info('System is forced active at the current time - ignoring network presence')
+        if self.zone_guard_start and self.zone_guard_end and self._in_guard_window():
+            log.info('In guard window - sending notifications')
+            return True
+        elif self._system_active_ip_based():
             return True
         else:
-            if self.ip_addresses:
-                system_active = self._system_active_ip_based()
-            else:
-                if self.network and self.presenceMacs:
-                    system_active = self._system_active_arp_based()
-            logger.info('Based on network presence should the email be sent %s', system_active)
-        return system_active
-
-    def _email_required(self, notify):
-        logger.info('Checking if email required')
-        if not self.send_email or not notify:
-            logger.info('Either email is disabled globally or is disabled for this task via command line parameters')
             return False
-        logger.info('Email required for this task')
-        return True
 
-    def _system_active_arp_based(self):
-        if not self.network or not self.presenceMacs:
-            return None
-        logger.info("Checking for presence via MAC address")
-        result = subprocess.Popen(['sudo', 'arp-scan', self.network], stdout=subprocess.PIPE,stderr=subprocess.STDOUT).stdout.readlines()
-        logger.info("result %s", result)
-        for addr in result:
-            for i in self.presenceMacs:
-                if i.lower() in addr.lower():
-                    logger.info('ARP entry found - someone is home')
-                    return False
-        logger.info('No ARP entry found - nobody is home - system is active')
-        return True
+    def _in_guard_window(self):
+        now = datetime.now()
+        start = now.hour >= self.zone_guard_start
+        end = now.hour < self.zone_guard_end
+        return start and end
 
     def _system_active_ip_based(self):
         if not self.ip_addresses:
-            logger.info("No IP addresses configured - skipping IP check")
+            log.info("No IP addresses configured - skipping IP check")
             return True
-        logger.info("Checking for presence via IP address")
-        addresses = self.ip_addresses.split(',')
+        addresses = self.ip_addresses
         for address in addresses:
-            #test_string = 'is up'
             test_string = 'bytes from'
-            results = subprocess.Popen(['ping', '-c1', address], stdout=subprocess.PIPE,stderr=subprocess.STDOUT).stdout.readlines()
-            #results = subprocess.Popen( ['nmap', '-sPn', address ], stdout=subprocess.PIPE,stderr=subprocess.STDOUT ).stdout.readlines()
-            logger.info("Nmap result %s", results)
+            results = subprocess.Popen(['ping', '-c1', address],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.STDOUT).stdout.readlines()
             for result in results:
                 if test_string in result:
-                    logger.info('IP detected - someone is home')
+                    log.info('IP detected - someone is home')
                     return False
-        logger.info('IP inactive - nobody is home - system is active')
+        log.info('IP inactive - nobody is home - system is active')
         return True
 
     def upload_media(self, media_file_path, notify):
         if self._system_active():
-            """Upload media file to the specified folder. Then optionally send
-            an email and optionally delete the local file."""
-            folder_resource = self._get_folder_resource()
-            if not folder_resource:
-                raise Exception('Could not find the %s folder' % self.folder)
+            link = self._upload_dropbox_file(media_file_path)
+            self._push_notice("{} - view at {}".format(self.message, self.folder_link))
 
-            doc = self._upload(media_file_path, folder_resource)
-
-            """Config file has email enable and it is set to true on the
-            command line"""
-            if self._email_required(notify):
-                media_link = None
-                for link in doc.link:
-                    if 'video.google.com' in link.href:
-                        media_link = link.href
-                        break
-                # Send an email with the link if found
-                msg = self.message
-                if media_link:
-                    msg += '\n\n' + media_link
-                self._send_email(msg)
-
-        if self.delete_files:
-            logger.info("Deleting: %s", media_file_path)
+        if self.cleanup:
+            log.info("Deleting: %s", media_file_path)
             os.remove(media_file_path)
 
     def send_start_event_email(self, notify):
         """Send an email showing that the event has started"""
-        if self._email_required(notify) and self._system_active():
+        if self._system_active():
             msg = self.event_started_message
-            msg += '\n\n' + self.google_drive_folder_link
-            self._send_email(msg)
+            msg += '\n\n' + self.folder_link
+            self._push_notice(msg)
 
 
 if __name__ == '__main__':
-    logger.info("Motion Notify script started")
-    try:
-        if len(sys.argv) < 3:
-            exit('Motion Notify - uploads media to Google Drive when network presence indicates nobody is home\n   by Andrew Dean, based on Google Drive Uploader Jeremy Blythe (http://jeremyblythe.blogspot.com)\n\n   Usage: uploader.py {config-file-path} {media-file-path} {send-email (1 if email required, if not, 0)}')
-        cfg_path = sys.argv[1]
+    parser = argparse.ArgumentParser(prog='MNotify - A notifier app for Motion',
+                                     description='Based on the Motion Notifier'
+                                     ' scripts developed by Andrew Dean.')
+    parser.add_argument('--media', '-m',
+                         help="Media file path to upload", default='')
+    parser.add_argument('--config', '-c',
+                        help="Specify configuration file to parse",
+                        default="/etc/motion/notify.conf")
+    parser.add_argument('--notify', '-n',
+                         help="Enable push-bullet notifications",
+                         action="store_true", default=True)
 
-        vid_path = sys.argv[2]
-        if sys.argv[3] == '1':
-            notify = True
-        else:
-            notify = False
-        if not os.path.exists(cfg_path):
-            exit('Config file does not exist [%s]' % cfg_path)
+    args = parser.parse_args()
 
-        if vid_path == 'None':
-                MotionNotify(cfg_path).send_start_event_email(notify)
-                exit('Start event triggered')
+    if not os.path.exists(args.config):
+        log.critical('Config file does not exist [{}]'.format(args.config))
+        sys.exit(1)
 
-        if not os.path.exists(vid_path):
-            exit('Video file does not exist [%s]' % vid_path)
+    if args.media == 'None':
+        MotionNotify(args.config).send_start_event_email(notify)
+        exit('Start event triggered')
 
-        MotionNotify(cfg_path).upload_media(vid_path, notify)
-    except gdata.client.BadAuthentication:
-        exit('Invalid user credentials given.')
-    except gdata.client.Error:
-        exit('Login Error')
-    except Exception as e:
-        exit('Error: [%s]' % e)
+    if not os.path.exists(args.media):
+        log.critical('Video file does not exist [{}]'.format(args.media))
+        sys.exit(1)
+
+    MotionNotify(args.config, args.notify).upload_media(args.media, args.notify)
+
